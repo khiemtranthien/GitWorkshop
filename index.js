259bca54fbd4bea240597d367f11e2a3d561a8d0
hoatt1097 <hoatt1097@gmail.com>
2020-03-17 23:02:18 +0700

    Add endpoint get user detail

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/user', (req, res) => res.send('{"user_id" : 1, "username" : "john", "password" : "123456"}'));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
