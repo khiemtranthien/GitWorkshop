271b1b5ce4130de62c21563a728fda519d597048
Khiem Tran <khiemtranthien1411@gmail.com>
2020-03-17 23:17:53 +0700

    Add sale endpoint

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.get('/product', (req, res) => res.send('Product 1s'));
+app.get('/product', (req, res) => res.send('Product 1'));
+
+app.get('/sale', (req, res) => res.send('Sale product 1'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
