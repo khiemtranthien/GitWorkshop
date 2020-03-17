9f9eb49454b9ecdcc23ef79d454a5dc9d35f8302
Tran Thien Khiem <khiemtranthien1411@gmail.com>
2020-03-17 23:55:57 +0700

    merge develop to master include user, sale, product endpoint

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/product', (req, res) => res.send('Product 1'));
+app.get('/sale', (req, res) => res.send('Sale product 1'));
+app.get('/user', (req, res) => res.send('{"user_id" : 1, "username" : "john", "password" : "123456"}'));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
