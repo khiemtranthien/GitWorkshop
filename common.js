82fb73e6da6dd74b5a633d08517ba608b7ac5db1
Tran Thien Khiem <khiemtranthien1411@gmail.com>
2020-03-18 00:04:40 +0700

    Add common util, currency endpoint

---
+++ common.js
@@ -0,0 +1,3 @@
+exports.getCurrency = function () {
+    return "USD";
+}
--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const common = require('./common')
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/product', (req, res) => res.send('Product 1'));
 app.get('/sale', (req, res) => res.send('Sale product 1'));
 app.get('/user', (req, res) => res.send('{"user_id" : 1, "username" : "john", "password" : "123456"}'));
+
+app.get('/currency', (req, res) => res.send(common.getCurrency()));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
